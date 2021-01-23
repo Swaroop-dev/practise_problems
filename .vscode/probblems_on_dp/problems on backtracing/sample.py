a7bb73b9d7212e96b14a54a7a75fddd4251fd5f6
Swaroop <swaroopaee98@gmail.com>
2021-01-23 20:14:07 +0530

    rat in a maze using backtracking

--- .vscode/probblems_on_dp/problems on backtracing/sample.py
+++ .vscode/probblems_on_dp/problems on backtracing/sample.py
@@ -1,5 +1,6 @@
 #backtracking
 #permmutation of a string using backtracking
+#rat in a maze problem
     
 
 def permutation_using_backtracking(s,j):
@@ -28,3 +29,51 @@ def permutation_using_backtracking(s,j):
             s[:j]+x+s[j+1:i]+y+s[i+1:]
         
 # permutation_using_backtracking('ABCD',0)        
+
+
+
+#rat in the maze problem
+total_paths=0
+x=[[0,0,1,0,0,1,0],[1,0,1,1,0,0,0],[0,0,0,0,1,0,1],[1,0,1,0,0,0,0],[1,0,1,1,0,1,0],[1,0,0,0,0,1,0],[1,1,1,1,0,0,0]]
+
+n=len(x[0])
+m=len(x)
+
+
+visited=[[False]*n for _ in range(m)]
+
+
+def isSafe(i,j):
+    return i>=0 and j>=0 and i<m and j<n and visited[i][j]==False
+
+def helper(i,j):
+    global total_paths
+    if i==m-1 and j==n-1:
+        total_paths=total_paths+1
+        return
+    if (not isSafe(i,j)):
+        return
+    visited[i][j]=True
+    #down
+    if i+1<m and  x[i+1][j]==0:
+        helper(i+1,j)
+    #up
+    if i-1>=0 and   x[i-1][j]==0:
+        helper(i-1,j)
+    #right
+    if j+1<n and  x[i][j+1]==0:
+        helper(i,j+1)
+    #left 
+    if j-1>=0 and x[i][j-1]==0:
+        helper(i,j-1)  
+        
+    visited[i][j]=False
+    return
+    
+def rat_in_a_maze():
+    helper(0, 0)
+    return total_paths
+
+print(rat_in_a_maze())
+
+   
