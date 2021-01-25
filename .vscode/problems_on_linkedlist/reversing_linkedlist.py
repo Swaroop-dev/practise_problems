5c7b5210cec681c45fc630d54e70c2c1b01bad0b
Swaroop <swaroopaee98@gmail.com>
2021-01-25 19:07:51 +0530

    linked list folding  problem

--- .vscode/problems_on_linkedlist/reversing_linkedlist.py
+++ .vscode/problems_on_linkedlist/reversing_linkedlist.py
@@ -1,3 +1,9 @@
+#reversing the linked list
+#rerversing the part of linked list
+#circular deletion of nodes
+#folding of linked list
+#swapping the linked list
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -17,26 +23,45 @@ class linkedlist:
                 cur = cur.next
             cur.next = Node(n1)
 
-    def printing_node(self):
-        if self.head is None:
-            return None
-        cur = self.head
+    def printing_node(self,n1=None):
+        
+        if n1 is None:
+            if self.head is None:
+                return None
+            cur = self.head
+            while cur:
+                print(cur.data, end="->")
+                cur = cur.next
+            return 
+        cur=n1
         while cur:
-            print(cur.data, end="->")
-            cur = cur.next
+            print(cur.data,end="->")
+            cur=cur.next
+            
+        
 
-    def reversing_linkedlist(self):
-        if self.head is None:
-            return None
-        cur = self.head
-        prev = None
+    def reversing_linkedlist(self,n1=None):
+        if n1 is None:
+            if self.head is None:
+                return None
+            cur = self.head
+            prev = None
 
+            while cur:
+                nxt = cur.next
+                cur.next = prev
+                prev = cur
+                cur = nxt
+            self.head = prev
+            return
+        cur=n1
+        prev=None
         while cur:
-            nxt = cur.next
-            cur.next = prev
-            prev = cur
-            cur = nxt
-        self.head = prev
+            nxt=cur.next
+            cur.next=prev
+            prev=cur
+            cur=nxt
+        return prev    
 
     def deleting_node(self, n1):
         if self.head is None:
@@ -87,7 +112,45 @@ class linkedlist:
             cur1.next = None
             self.printing_node()
             print('\n')
+    
+    def folding_the_linked_list(self):
+        #steps to solve
+        #find the middle element
+        #slit into ll
+        #reverse the second split
+        #merge them
 
+        cur=self.head
+        slow=fast=cur
+        while fast and fast.next:
+            prev = slow
+            slow=slow.next
+            fast=fast.next.next  
+        print(slow.data) 
+        temp=slow.next     
+        slow.next=None
+        self.printing_node(n1=temp)
+        print('\n')
+        self.printing_node()
+        print('\n')
+        #reversing list
+        first=self.head
+        second=self.reversing_linkedlist(n1=temp)
+        #merging the linked list
+        while first.next:
+            temp=first.next
+            first.next=second
+            first=temp
+            temp=second.next
+            second.next=first
+            second=temp    
+        self.printing_node()    
+           
+    
+    def finding_the_intersection(self,l):
+        
+        return
+    
     def reversing_part_of_the_linkedlist(self, index1, index2):
         cur = self.head
         inc = 1
@@ -116,9 +179,11 @@ l1.adding_node(4)
 l1.adding_node(5)
 l1.adding_node(6)
 l1.adding_node(7)
-l1.printing_node()
-print('\n')
-l1.deleting_mid_element()
+# l1.adding_node(8)
+l1.folding_the_linked_list()
+# l1.printing_node()
+# print('\n')
+# l1.deleting_mid_element()
 # l1.reversing_part_of_the_linkedlist(2, 5)
 # print('\n')
 # l1.printing_node()
